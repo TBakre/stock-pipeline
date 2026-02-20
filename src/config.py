213fc7c81c4e6ddd6f590f8d837755e6fa7791e9
Tobi Bakre <tobi@MacBookAir.lan>
2026-02-19 22:00:11 -0500

    feat: add stock data fetcher with Alpha Vantage API

---
+++ src/config.py
@@ -0,0 +1,21 @@
+import os 
+from dotenv import load_dotenv 
+
+#loading variables from my .env file into the program  
+load_dotenv() 
+
+#API Configuration 
+API_KEY = os.getenv("ALPHA_VANTAGE_API_KEY") 
+BASE_URL = "https://www.alphavantage.co/query" 
+
+# Stocks we want to track 
+STOCKS = ["AAPL", "GOOGL", "MSFT", "AMZN"]
+
+# Database configuration  
+DB_CONFIG = { 
+    "host" : "localhost", 
+    "databse": "stock_pipeline" , 
+    "user" : "postgres" , 
+    "port" : 5432
+
+}
---
+++ src/fetcher.py
@@ -0,0 +1,54 @@
+import requests 
+import pandas as pd 
+from datetime import datetime  
+import time
+from src.config import API_KEY, BASE_URL, STOCKS 
+
+def fetch_stock_quote(symbol): 
+
+    """ 
+    Fetches the current price and vlume for a single stock symbol.
+    """ 
+
+    # parameters sent to API  
+    params = { 
+        "function" : "GLOBAL_QUOTE" , 
+        "symbol" : symbol, 
+        "apikey" : API_KEY
+    } 
+
+    # API call
+    response = requests.get(BASE_URL, params=params)
+    
+    # Convert the response to a Python dictionary
+    data = response.json()
+    
+    # just the quote data we need
+    quote = data["Global Quote"]
+    
+    # Return an organized dictionary with just what we need
+    return {
+        "symbol": symbol,
+        "price": float(quote["05. price"]),
+        "volume": int(quote["06. volume"]),
+        "change_percent": quote["10. change percent"],
+        "timestamp": datetime.now()
+    }
+
+def fetch_all_stocks():
+    """
+    Loops through all stocks in our config and fetches each one.
+    Returns a pandas DataFrame — a table of all our stock data.
+    """
+    
+    all_quotes = []
+    
+    for symbol in STOCKS:
+        print(f"Fetching data for {symbol}...")
+        quote = fetch_stock_quote(symbol)
+        all_quotes.append(quote) 
+        time.sleep(15)
+    
+    # Convert our list of dictionaries into a DataFrame (a table)
+    df = pd.DataFrame(all_quotes)
+    return df
